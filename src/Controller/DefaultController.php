99c33c68aacacec53fd36a8277733cc07ce22865
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2020-01-30 23:28:01 +0100

    Diverse ændringer omkring locale

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
   /**
@@ -25,8 +27,20 @@ class DefaultController extends AbstractController
 	 *
 	 **/
 
-	public function index(SessionInterface $session)
+	public function index(Request $request)
 	{
+		$request->getSession()->set('_locale', 'da');
+
+	  $url = $this->generateUrl('calendar1', ['aarstal' => 1735]);
+
+    return $this->redirectToRoute('calendar1', ['aarstal' => 1736]);
+
+    return $this->redirectToRoute('calendar1');
+
+		$response = new RedirectResponse('calendar');
+		$response->send();
+
+
 		return $this->render('default/index.html.twig', [
 		'controller_name' => 'DefaultController',
 		]);
