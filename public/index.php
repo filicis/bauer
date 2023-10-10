70b70d31b47cc257cf8bfc9fbd41752d9c929d24
michael <mlr@dadlnet.dk>
2023-10-10 17:00:22 +0200

    recipes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 use App\Kernel;
+<<<<<<< ours
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 require dirname(__DIR__).'/config/bootstrap.php';
@@ -18,3 +19,11 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+=======
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+>>>>>>> theirs
